21e0d721d2a43630a44d2d413e188d5375214c88
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-26 18:10:47 +0200

    exo3

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+<?php
+//création du tableau month
+$month = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+echo $month[5]; //affichage de la 6eme ligne du tableau (index 5)
+?>
+    </p>
+  </body>
+</html>
